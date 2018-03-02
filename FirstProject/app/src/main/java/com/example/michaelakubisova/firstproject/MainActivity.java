6df9ff973a64cce4141c7d6c26d98bff15e284ef
Miska <miska@Michaela-iMac.local>
2018-03-02 22:06:21 +0100

    retrieving and saving data between activities

--- FirstProject/app/src/main/java/com/example/michaelakubisova/firstproject/MainActivity.java
+++ FirstProject/app/src/main/java/com/example/michaelakubisova/firstproject/MainActivity.java
@@ -5,9 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button goToSecondActivity;
+    private final int REQUEST_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +19,27 @@ public class MainActivity extends AppCompatActivity {
         goToSecondActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("Message", "Hello from first activity");
+                intent.putExtra("SecondMessage", "Hello again");
+                intent.putExtra("Value", 123);
+
+                //startActivity(intent);
+                startActivityForResult(intent, REQUEST_CODE);
+
+
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE){
+            if (resultCode == RESULT_OK){
+                String result = data.getStringExtra("returnData");
+                Toast.makeText(MainActivity.this, result, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
--- FirstProject/app/src/main/java/com/example/michaelakubisova/firstproject/SecondActivity.java
+++ FirstProject/app/src/main/java/com/example/michaelakubisova/firstproject/SecondActivity.java
@@ -1,13 +1,40 @@
 package com.example.michaelakubisova.firstproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    private TextView showMessage;
+    private Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Bundle extras = getIntent().getExtras();
+
+        showMessage = findViewById(R.id.textView);
+
+        if(extras != null){
+            String message = (String) extras.get("Message");
+
+            showMessage.setText(message);
+        }
+
+        backButton = findViewById(R.id.button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent returnIntent = getIntent();
+                returnIntent.putExtra("returnData", "From Second Activity");
+                setResult(RESULT_OK, returnIntent);
+                finish();
+            }
+        });
     }
 }
