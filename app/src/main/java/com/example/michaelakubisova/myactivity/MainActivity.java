ff62af93ef00df6c3bf1263f30f4cb958f7a0a0b
Kubišová Michaela <michaela.kubisova@artin.cz>
2018-03-02 14:49:02 +0100

    first changes

--- app/src/main/java/com/example/michaelakubisova/myactivity/MainActivity.java
+++ app/src/main/java/com/example/michaelakubisova/myactivity/MainActivity.java
@@ -1,39 +1,28 @@
 package com.example.michaelakubisova.myactivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity  {
+    private Button goToSecondButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        goToSecondButton = findViewById(R.id.showButtonID);
+        goToSecondButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+            }
+        });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-    @Override
-    protected void onPostResume() {
-        super.onPostResume();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
 }
