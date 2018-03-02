08628f292fe461105a34a74862377861f766d6c3
Kubišová Michaela <michaela.kubisova@artin.cz>
2018-03-02 14:09:27 +0100

    Initial commit

---
+++ app/src/main/java/com/example/michaelakubisova/myactivity/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.michaelakubisova.myactivity;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
